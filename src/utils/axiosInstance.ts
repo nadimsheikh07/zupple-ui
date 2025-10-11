ab39a85cb13265b2bab806695e09fd7256083941
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2025-10-11 16:24:17 +0530

    fix next auth

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
 // Axios instance
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   // Axios configuration options here
 });
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const session = await getSession();
+    if (session?.accessToken) {
+      config.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 // Interceptor to handle 301 redirects
 axiosInstance.interceptors.response.use(
