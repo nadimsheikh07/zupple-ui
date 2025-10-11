a72b0bb403fdf13fb4c9ab9865b833ef3f648fc2
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2025-10-11 16:22:52 +0530

    NEXT_PUBLIC_AUTH_URL

--- src/utils/authAxiosInstance.ts
+++ src/utils/authAxiosInstance.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
 // Axios instance
 const authAxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
   // Axios configuration options here
 });
+
+// Request interceptor
+authAxiosInstance.interceptors.request.use(
+  async (config) => {
+    const session = await getSession();
+    if (session?.accessToken) {
+      config.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 // Interceptor to handle 301 redirects
 authAxiosInstance.interceptors.response.use(
